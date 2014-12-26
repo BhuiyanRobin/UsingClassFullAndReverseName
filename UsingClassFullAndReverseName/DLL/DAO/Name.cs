055b32229526edd3d2702fc19880189bb907e6b5
bhuiyanrobin <bhuiyanrobin93@live.com>
2014-12-26 13:39:13 +0600

    int ma
    Signed-off-by: bhuiyanrobin <bhuiyanrobin93@live.com>

---
+++ UsingClassFullAndReverseName/DLL/DAO/Name.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingClassFullAndReverseName.DLL.DAO
+{
+    class Name
+    {
+        public string firstName;
+        public string lastName;
+        public string middleName;
+
+        public string GetFullName()
+        {
+            return firstName + " " + middleName + " " + lastName;
+        }
+
+        public string GetReverseName()
+        {
+            string fullName = GetFullName();
+
+            char[] cArray = fullName.ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+            return reverse;
+
+        }
+    }
+}
---
+++ UsingClassFullAndReverseName/UI/Person UI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UsingClassFullAndReverseName.DLL.DAO;
+
+namespace UsingClassFullAndReverseName
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Name aName=new Name();
+        void TakeInput()
+        {
+            if (firstNameTextBox == null || middleNameTextBox.Text == null || lstNameTextBox.Text == null)
+            {
+                MessageBox.Show("Please fill all the text box");
+            }
+            else
+            {
+                aName.firstName = firstNameTextBox.Text;
+                aName.middleName = middleNameTextBox.Text;
+                aName.lastName = lstNameTextBox.Text;
+            }
+           
+        }
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            TakeInput();
+            fullNameTextBox.Text = aName.GetFullName();
+            reverseNameTextBox.Text = aName.GetReverseName();
+        }
+    }
+}
